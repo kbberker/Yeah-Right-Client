dc8a9e78b7b46d8f226a73d398101879b8fe3635
Kenan Berker <kenan.berker@ocado.com>
2020-02-19 08:05:02 +0000

    refactor: Make some eslint changes

--- src/API.js
+++ src/API.js
@@ -1,79 +1,77 @@
 class API {
   static init() {
-    this.baseURL = process.env.REACT_APP_API_URL
-    this.signinURL = this.baseURL + '/signin'
+    this.baseURL = process.env.REACT_APP_API_URL;
+    this.signinURL = `${this.baseURL}/signin`;
   }
-  
+
   static createNewRound(gameId) {
-    return fetch(this.baseURL + "/rounds", {
-      method: "POST",
-      headers: { 'Content-Type': "application/json" },
+    return fetch(`${this.baseURL}/rounds`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        game_id: gameId
-      })
-    }).then(resp => resp.json())
+        game_id: gameId,
+      }),
+    }).then((resp) => resp.json());
   }
 
   static createPlayerAndJoinGame(playerName, gameName) {
-    console.log({"createPlayerAndJoinGame": [playerName, gameName]})
-    return fetch(this.baseURL + "/players", {
-      method: "POST",
-      headers: { 'Content-Type': "application/json"},
+    return fetch(`${this.baseURL}/players`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         name: playerName,
-        game_name: gameName
-      })
-    }).then(resp => resp.json())
+        game_name: gameName,
+      }),
+    }).then((resp) => resp.json());
   }
-  
+
   static getListOfGames() {
-    return fetch(this.baseURL + "/games")
-      .then(resp => resp.json())
+    return fetch(`${this.baseURL}/games`)
+      .then((resp) => resp.json());
   }
 
   static getPlayersInGame(gameId) {
-    return fetch(this.baseURL + "/games_players", {
-      method: "POST",
-      headers: { 'Content-Type': "application/json" },
-      body: JSON.stringify({ game_id: gameId })
+    return fetch(`${this.baseURL}/games_players`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ game_id: gameId }),
     })
-      .then(resp => resp.json())
+      .then((resp) => resp.json());
   }
 
   static getRoundAnswers(roundId) {
     return fetch(`${this.baseURL}/rounds/${roundId}`)
-      .then(resp => resp.json())
+      .then((resp) => resp.json());
   }
 
   static hasGameStarted(gameId) {
     return fetch(`${this.baseURL}/games/${gameId}`)
-      .then(resp => resp.json())
+      .then((resp) => resp.json());
   }
 
   static setDasher(newDasher) {
-    return fetch(this.baseURL + "/set_dasher", {
-      method: "POST",
-      headers: { 'Content-Type': "application/json" },
+    return fetch(`${this.baseURL}/set_dasher`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        new_dasher: newDasher
-      })
-    }).then(resp => resp.json())
+        new_dasher: newDasher,
+      }),
+    }).then((resp) => resp.json());
   }
 
   static submitAnswer(answerText, roundId, playerId) {
-    return fetch(this.baseURL + "/answers", {
-      method: "POST",
-      headers: { 'Content-Type': "application/json" },
+    return fetch(`${this.baseURL}/answers`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         text: answerText,
         round_id: roundId,
-        player_id: playerId
-      })
-    }).then(resp => resp.json())
+        player_id: playerId,
+      }),
+    }).then((resp) => resp.json());
   }
-
 }
 
-API.init()
+API.init();
 
-export default API
+export default API;
--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import React, { Component } from "react";
-import HomeScreen from "./components/HomeScreen";
-import CreateScreen from "./components/CreateScreen";
-import JoinScreen from "./components/JoinScreen";
-import WaitingScreen from "./components/WaitingScreen";
-import AnswerScreen from "./components/AnswerScreen";
-import AnswerWaitingScreen from "./components/AnswerWaitingScreen";
-import VotingScreen from "./components/VotingScreen";
-import ScoreScreen from "./components/ScoreScreen";
-import API from "./API";
-import logo from "./images/yeah-right-logo.svg";
+import React, { Component } from 'react';
+import HomeScreen from './components/HomeScreen';
+import CreateScreen from './components/CreateScreen';
+import JoinScreen from './components/JoinScreen';
+import WaitingScreen from './components/WaitingScreen';
+import AnswerScreen from './components/AnswerScreen';
+import AnswerWaitingScreen from './components/AnswerWaitingScreen';
+import VotingScreen from './components/VotingScreen';
+import ScoreScreen from './components/ScoreScreen';
+import API from './API';
+import logo from './images/yeah-right-logo.svg';
 
 class App extends Component {
   state = {
     currentDasher: {},
     currentRound: {},
-    currentScreen: "home",
+    currentScreen: 'home',
     game: {},
     gamesPlayers: [],
     player: {},
-    answers: []
+    answers: [],
   };
 
   componentDidUpdate() {
     console.log({
-      "componentDidUpdate before setGameScreen": this.state.currentScreen
+      'componentDidUpdate before setGameScreen': this.state.currentScreen,
     });
     this.setGameScreen();
   }
@@ -32,13 +32,13 @@ class App extends Component {
     const { currentDasher, player } = this.state;
     // ? Might be better to change this based on an argument then state
     switch (this.state.currentScreen) {
-      case "home":
+      case 'home':
         return <HomeScreen createOrJoin={this.changeGameScreenState} />;
-      case "create":
+      case 'create':
         return <CreateScreen createGame={this.createGame} />;
-      case "join":
+      case 'join':
         return <JoinScreen createGame={this.createGame} />;
-      case "waiting":
+      case 'waiting':
         return (
           <WaitingScreen
             gameId={this.state.game.id}
@@ -46,167 +46,162 @@ class App extends Component {
             joinGame={this.joinGame}
           />
         );
-      case "answer":
+      case 'answer':
         return currentDasher.id === player.id ? (
-          <AnswerScreen submitAnswer={this.submitAnswer} isDasher={true} />
+          <AnswerScreen submitAnswer={this.submitAnswer} isDasher />
         ) : (
-            <AnswerScreen submitAnswer={this.submitAnswer} isDasher={false} />
-          );
-      case "answer-waiting":
+          <AnswerScreen submitAnswer={this.submitAnswer} isDasher={false} />
+        );
+      case 'answer-waiting':
         return currentDasher.id === player.id ? (
           <AnswerWaitingScreen
             currentRoundId={this.state.currentRound.id}
             renderVotingScreen={this.renderVotingScreen}
-            isDasher={true}
+            isDasher
             joinGame={this.joinGame}
           />
         ) : (
-            <AnswerWaitingScreen
-              currentRoundId={this.state.currentRound.id}
-              renderVotingScreen={this.renderVotingScreen}
-              isDasher={false}
-              joinGame={this.joinGame}
-            />
-          );
-      case "voting":
+          <AnswerWaitingScreen
+            currentRoundId={this.state.currentRound.id}
+            renderVotingScreen={this.renderVotingScreen}
+            isDasher={false}
+            joinGame={this.joinGame}
+          />
+        );
+      case 'voting':
         return currentDasher.id === player.id ? (
           <VotingScreen
             answers={this.state.answers}
             players={this.state.gamesPlayers}
-            isDasher={true}
+            isDasher
             calculateScores={this.calculateScores}
           />
         ) : (
-            <VotingScreen
-              answers={this.state.answers}
-              players={this.state.gamesPlayers}
-              isDasher={false}
-              calculateScores={this.calculateScores}
-            />
-          );
-      case "scores":
+          <VotingScreen
+            answers={this.state.answers}
+            players={this.state.gamesPlayers}
+            isDasher={false}
+            calculateScores={this.calculateScores}
+          />
+        );
+      case 'scores':
         return currentDasher.id === player.id ? (
           <ScoreScreen
             answers={this.state.answers}
             players={this.state.gamesPlayers}
-            isDasher={true}
+            isDasher
             currentDasher={this.state.currentDasher}
             pickNewDasher={this.pickNewDasher}
           />
         ) : (
-            <ScoreScreen
-              answers={this.state.answers}
-              players={this.state.gamesPlayers}
-              isDasher={false}
-              currentDasher={this.state.currentDasher}
-              pickNewDasher={this.pickNewDasher}
-            />
-          );
+          <ScoreScreen
+            answers={this.state.answers}
+            players={this.state.gamesPlayers}
+            isDasher={false}
+            currentDasher={this.state.currentDasher}
+            pickNewDasher={this.pickNewDasher}
+          />
+        );
       default:
         break;
     }
   };
 
-  changeGameScreenState = nextScreen => {
+  changeGameScreenState = (nextScreen) => {
     this.setState({ currentScreen: nextScreen });
   };
 
   createGame = (playerName, gameName) => {
-    API.createPlayerAndJoinGame(playerName, gameName).then(player => {
+    API.createPlayerAndJoinGame(playerName, gameName).then((player) => {
       this.setState({
         game: player.game,
-        player: player,
-        currentScreen: "waiting"
+        player,
+        currentScreen: 'waiting',
       });
     });
   };
 
   startGame = () => {
     API.createNewRound(this.state.game.id)
-      .then(round => {
-        console.log({ "App.startGame round returned": round });
-        const newDasher =
-          round.players[Math.floor(Math.random() * round.players.length)];
-        this.setState({ currentRound: round, currentScreen: "answer" });
+      .then((round) => {
+        console.log({ 'App.startGame round returned': round });
+        const newDasher = round.players[Math.floor(Math.random() * round.players.length)];
+        this.setState({ currentRound: round, currentScreen: 'answer' });
         return newDasher;
       })
-      .then(newDasher => API.setDasher(newDasher))
-      .then(resp => {
+      .then((newDasher) => API.setDasher(newDasher))
+      .then((resp) => {
         this.setState({
           currentDasher: resp.new_dasher,
-          gamesPlayers: [...resp.non_dashers, resp.new_dasher]
+          gamesPlayers: [...resp.non_dashers, resp.new_dasher],
         });
       });
   };
 
   joinGame = () => {
-    API.hasGameStarted(this.state.game.id).then(gameRounds => {
+    API.hasGameStarted(this.state.game.id).then((gameRounds) => {
       gameRounds.length === 0
-        ? console.log("Game not started yet")
+        ? console.log('Game not started yet')
         : this.setState({
-          currentScreen: "answer",
+          currentScreen: 'answer',
           currentRound: gameRounds[gameRounds.length - 1],
           gamesPlayers: gameRounds[gameRounds.length - 1].players,
           currentDasher: gameRounds[gameRounds.length - 1].players.filter(
-            player => player.is_dasher === true
-          )[0]
+            (player) => player.is_dasher === true,
+          )[0],
         });
     });
   };
 
-  submitAnswer = answerText => {
+  submitAnswer = (answerText) => {
     API.submitAnswer(
       answerText,
       this.state.currentRound.id,
-      this.state.player.id
-    ).then(this.setState({ currentScreen: "answer-waiting" }));
+      this.state.player.id,
+    ).then(this.setState({ currentScreen: 'answer-waiting' }));
   };
 
-  renderVotingScreen = roundsAnswers => {
+  renderVotingScreen = (roundsAnswers) => {
     this.setState({
       answers: roundsAnswers,
-      currentScreen: "voting"
+      currentScreen: 'voting',
     });
   };
 
-  calculateScores = votes => {
+  calculateScores = (votes) => {
     const { answers, currentDasher, gamesPlayers } = this.state;
     const copyOfDasher = JSON.parse(JSON.stringify(currentDasher));
     const copyOfGamesPlayers = JSON.parse(JSON.stringify(gamesPlayers));
     // TODO Set everyone's roundScore to 0
     copyOfDasher.roundScore = 0;
-    copyOfGamesPlayers.forEach(player => {
+    copyOfGamesPlayers.forEach((player) => {
       player.roundScore = 0;
     });
 
     // TODO Find which answer is the dashers.
     const dasherAnswer = answers.find(
-      answer => answer.player_id === copyOfDasher.id
+      (answer) => answer.player_id === copyOfDasher.id,
     );
     // TODO iterate over each answerId in votes
-    Object.keys(votes).forEach(answerId => {
+    Object.keys(votes).forEach((answerId) => {
       if (parseInt(answerId) === dasherAnswer.id) {
         // If no-one votes for the correct answer, Dasher gets 2 points
         // Otherwise each player who voted for it gets 1 point
         if (votes[answerId].length === 0) {
           copyOfDasher.roundScore += 2;
         } else {
-          votes[answerId].forEach(player => {
-            let playerIndex = copyOfGamesPlayers.findIndex(aPlayer => {
-              return aPlayer.id === player.id;
-            });
+          votes[answerId].forEach((player) => {
+            const playerIndex = copyOfGamesPlayers.findIndex((aPlayer) => aPlayer.id === player.id);
             copyOfGamesPlayers[playerIndex].roundScore += 1;
           });
         }
       } else {
-        let score = votes[answerId].length;
+        const score = votes[answerId].length;
         // TODO find which player gave that answer
-        let playersAnswer = answers.find(
-          answer => answer.id === parseInt(answerId)
+        const playersAnswer = answers.find(
+          (answer) => answer.id === parseInt(answerId),
         );
-        let playerIndex = copyOfGamesPlayers.findIndex(aPlayer => {
-          return playersAnswer.player_id === aPlayer.id;
-        });
+        const playerIndex = copyOfGamesPlayers.findIndex((aPlayer) => playersAnswer.player_id === aPlayer.id);
         copyOfGamesPlayers[playerIndex].roundScore += score;
       }
     });
@@ -214,22 +209,22 @@ class App extends Component {
     this.setState({
       gamesPlayers: copyOfGamesPlayers,
       currentDasher: copyOfDasher,
-      currentScreen: "scores"
+      currentScreen: 'scores',
     });
   };
 
-  pickNewDasher = newDasher => {
+  pickNewDasher = (newDasher) => {
     API.createNewRound(this.state.game.id)
-      .then(round => {
-        console.log({ "App.pickNewDasher round returned": round });
-        this.setState({ currentRound: round, currentScreen: "answer" });
+      .then((round) => {
+        console.log({ 'App.pickNewDasher round returned': round });
+        this.setState({ currentRound: round, currentScreen: 'answer' });
         return newDasher;
       })
-      .then(newDasher => API.setDasher(newDasher))
-      .then(resp => {
+      .then((newDasher) => API.setDasher(newDasher))
+      .then((resp) => {
         this.setState({
           currentDasher: resp.new_dasher,
-          gamesPlayers: [...resp.non_dashers, resp.new_dasher]
+          gamesPlayers: [...resp.non_dashers, resp.new_dasher],
         });
       });
   };
