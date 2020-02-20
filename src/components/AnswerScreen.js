ccc3d1217444a4de6d8572a148e2e678a79fa6f7
Kenan Berker <kenan.berker@ocado.com>
2020-02-20 17:27:02 +0000

    fix: Small changes to linting issues

--- src/components/AnswerScreen.js
+++ src/components/AnswerScreen.js
@@ -1,54 +1,55 @@
-import React, { Component, Fragment } from 'react'
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap'
+import React, { Component, Fragment } from 'react';
+import {
+  Button, Form, FormGroup, Label, Input,
+} from 'reactstrap';
 
 
 class AnswerScreen extends Component {
   state = {
-    playerAnswerInput: "",
+    playerAnswerInput: '',
   }
 
 
   handlePlayerAnswerInputChange = (e) => {
-    this.setState({ playerAnswerInput: e.target.value })
+    this.setState({ playerAnswerInput: e.target.value });
   }
 
   handleGameNameInputChange = (e) => {
-    this.setState({ gameNameInput: e.target.value })
+    this.setState({ gameNameInput: e.target.value });
   }
 
   render() {
-    let { playerAnswerInput } = this.state
-    const { isDasher } = this.props
+    const { playerAnswerInput } = this.state;
+    const { isDasher } = this.props;
     return (
       <div className="content">
         {
           // maybe just isDasher && <h4>You're the Dasher</h4>
-          isDasher 
-          ? <h4>You're the Dasher</h4>
-          : null
+          isDasher
+            ? <h4>You're the Dasher</h4>
+            : null
         }
         <Form>
           <FormGroup>
             {
-              isDasher 
-              ? <Label for="playerAnswer">Enter the Real Answer Here:</Label>
-              : <Label for="playerAnswer">Enter Your Answer Here:</Label>
+              isDasher
+                ? <Label for="playerAnswer">Enter the Real Answer Here:</Label>
+                : <Label for="playerAnswer">Enter Your Answer Here:</Label>
             }
             <Input type="text" name="playerAnswer" id="playerAnswer" onChange={this.handlePlayerAnswerInputChange} />
           </FormGroup>
         </Form>
-        <Button 
-          outline 
-          color="primary" 
+        <Button
+          outline
+          color="primary"
           className="button"
           onClick={() => this.props.submitAnswer(playerAnswerInput)}
         >
           SUBMIT ANSWER
         </Button>
       </div>
-    )
+    );
   }
-
 }
 
 export default AnswerScreen;
